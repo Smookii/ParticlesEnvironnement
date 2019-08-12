ba55519444e62016024f245fb8d9f10ffa7c23a3
Dakota-low <axel.bentodasilva@gmail.com>
2019-08-12 06:20:37 -0500

    Try MRUA movement

--- particle.py
+++ particle.py
@@ -2,32 +2,37 @@ import random
 
 
 class Particle():
-    def __init__(self, startpos, initspeed, gravity, scatterx, scattery, col):
+    def __init__(self, startpos, initspeed, col):
+        self.startpos = [startpos[0],startpos[1]]
         self.pos = [startpos[0],startpos[1]]
-        self.speed = [initspeed[0]*2 + random.uniform(scatterx[0],scatterx[1]),initspeed[1]*2 + random.uniform(scattery[0],scattery[1])]
-        self.gravity = gravity
+        scatterx = [-20,20]
+        scattery = [-18,8]
+        self.start_speed = [initspeed[0]*2 + random.uniform(scatterx[0],scatterx[1]),initspeed[1]*2 + random.uniform(scattery[0],scattery[1])]        
+        self.actual_speed = list(self.start_speed)
+        self.gravity = [0,180]
         self.col = col
+        self.time = [0,0]
+        self.bounce_ratio = [1,1.5]
         self.out = False
         
+    
+    def update_speed(self):      
+        for i in range(0,2):
+            self.actual_speed[i] = self.start_speed[i] + self.gravity[i]*self.time[i]
+        
+        
+    def rebounce(self, max):        
+        for i in range(0,2):
+            if self.pos[i] < 0:                
+                self.startpos[i] = 0
+                self.start_speed[i] = -self.actual_speed[i]/2 * random.uniform(self.bounce_ratio[0],self.bounce_ratio[1])  
+                self.time[i] = 0
+            if self.pos[i] > max[i]:
+                self.startpos[i] = max[i]
+                self.start_speed[i] = -self.actual_speed[i]/2 * random.uniform(self.bounce_ratio[0],self.bounce_ratio[1])
+                self.time[i] = 0
 
-    def update(self,delta, xmax, ymax):
-        #if self.pos[1] < 0 or self.pos[1] > ymax or self.pos[0] < 0 or self.pos[0] > xmax:
-        #    self.out = True
-        bounce = [-92,130]
-        if self.pos[1] < 0 :
-            self.pos[1] = 0
-            self.speed[1] =- self.speed[1]/2 + random.uniform(bounce[0],bounce[1])
-        if self.pos[1] > ymax:
-            self.pos[1] = ymax
-            self.speed[1] =- self.speed[1]/2 + random.uniform(bounce[0],bounce[1])
-        if self.pos[0] < 0:
-            self.pos[0] = 0
-            self.speed[0] =- self.speed[0]/2 + random.uniform(bounce[0],bounce[1])      
-        if self.pos[0] > xmax:
-            self.pos[0] = xmax
-            self.speed[0] =- self.speed[0]/2 + random.uniform(bounce[0],bounce[1])
-
-
+    def color_by_y(self,ymax):
         ratio_color = (self.pos[1] / ymax) * 255
         if ratio_color < 0:
             ratio_color = 0
@@ -36,8 +41,26 @@ class Particle():
         self.col[0] = ratio_color
         self.col[1] = 255-ratio_color
 
+    def movement(self):
+        for i in range(0,2):
+            self.pos[i] = self.startpos[i] + self.start_speed[i]*self.time[i]+(self.gravity[i]*self.time[i]*self.time[i]/2)
 
-        self.pos[0] += self.speed[0]*delta
-        self.pos[1] += self.speed[1]*delta
-        self.speed[0] += self.gravity[0]
-        self.speed[1] += self.gravity[1]
+    def update_time(self, delta):
+        for i in range(0,2):
+            self.time[i] += delta
+
+    def check_speed(self, ymax):
+        for i in range(0,2):
+            if self.actual_speed[i] > -3 and self.actual_speed[i] < 3 and self.pos[1] > ymax -5:
+                self.out = True
+
+        
+    def update(self,delta, xmax, ymax):
+        self.update_time(delta)
+        self.update_speed()
+        self.rebounce(max=[xmax,ymax])
+        self.color_by_y(ymax)
+        self.movement()
+        self.check_speed(ymax)
+
+        
